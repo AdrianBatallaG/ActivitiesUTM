767b2fc2051beab50be8481e508552efce0b0e0e
AdrianBatallaG <149129067+AdrianBatallaG@users.noreply.github.com>
2025-04-28 19:29:08 -0500

    Final files uploaded to be posted.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+
+const CACHE_NAME = "actividades-utm-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/graduation.png",
+  "/curriculum.png",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
